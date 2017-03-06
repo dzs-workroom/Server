5f1e2c8e4213f4e14f76df855f6e14d38c70a141
shzsofter <suliang100@hotmail.com>
2017-03-06 07:34:20 +0000

    lib and webl.xml

--- Server/src/com/shzsoft/resource/RootResource.java
+++ Server/src/com/shzsoft/resource/RootResource.java
@@ -11,6 +11,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 import com.shzsoft.model.Element;
 
@@ -19,45 +21,51 @@ import java.util.List;
 import java.util.Map;
 
 @Path("/resource")
-@Consumes({"application/json"})
-@Produces({"application/json"})
+// @Consumes({"application/json"})
+// @Produces({"application/json"})
 public class RootResource {
 
+    // @GET
+    // @Path("/list")
+    // public List<Element> resGet(
+    //         @QueryParam("conditions") Map conditions) {
+    //     return new ArrayList(){
+    //     	{
+    //     		add(1);
+    //     		add(2);
+    //     	}
+    //     };
+    // }
+
+    // @GET
+    // @Path("/{elmID}")
+    // public Element wizardGet(
+    //         @QueryParam("elmID") String elmID) {
+    //     return new Element();
+    // }
+
+    // @POST
+    // @Path("/creation")
+    // public Response wizardPost(Element body) {
+    //     return Response.ok().entity(body).build();
+    // }
+
+    // @PUT
+    // @Path("/{wizardID}")
+    // public Response wizardPut(Element body) {
+    //     return Response.ok().entity(body).build();
+
+    // }
+
+    // @DELETE
+    // @Path("/{wizardID}")
+    // public Response wizardWizardIDDelete(@PathParam("wizardID") Long wizardID) {
+    //     return Response.ok().entity("").build();
+    // }
+    
     @GET
-    @Path("/list")
-    public List<Element> resGet(
-            @QueryParam("conditions") Map conditions) {
-        return new ArrayList(){
-        	{
-        		add(1);
-        		add(2);
-        	}
-        };
-    }
-
-    @GET
-    @Path("/{elmID}")
-    public Element wizardGet(
-            @QueryParam("elmID") String elmID) {
-        return new Element();
-    }
-
-    @POST
-    @Path("/creation")
-    public Response wizardPost(Element body) {
-        return Response.ok().entity(body).build();
-    }
-
-    @PUT
-    @Path("/{wizardID}")
-    public Response wizardPut(Element body) {
-        return Response.ok().entity(body).build();
-
-    }
-
-    @DELETE
-    @Path("/{wizardID}")
-    public Response wizardWizardIDDelete(@PathParam("wizardID") Long wizardID) {
-        return Response.ok().entity("").build();
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getIt() {
+        return "Got it!";
     }
 }
